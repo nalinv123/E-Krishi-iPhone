58f10b4e2651d4520ef2549bb9904e8b1fc704f5
nalinv123 <nalinvaidya@gmail.com>
2019-11-17 18:18:18 +0530

    Initial Commit

---
+++ E-Krishi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  E-Krishi
+//
+//  Created by Nalin on 17/11/19.
+//  Copyright © 2019 Nalin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
